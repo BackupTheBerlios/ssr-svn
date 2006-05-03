d863d34f6eb425a846b4e6343115cb6ff24e9681
rjack <rjack@d2ba497e-da12-0410-9649-cf4375da043f>
2006-05-03 08:58:54 +0000

    Varo della nave: commit iniziale

---
+++ trunk/SongShuffler/src/songshuffler/core/CandidateList.java
@@ -0,0 +1,16 @@
+package songshuffler.core;
+
+
+public interface CandidateList {
+    void addItem();
+    
+    void addList();
+    
+    void getRandomItem();
+    
+    void removeItem();
+    
+    void reset();
+    
+    void resetItemState();
+}
