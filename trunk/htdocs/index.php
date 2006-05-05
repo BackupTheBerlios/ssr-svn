cd26b977789c85b5c4d9d9c48044d8d9aa55b4b9
rjack <rjack@d2ba497e-da12-0410-9649-cf4375da043f>
2006-05-05 19:57:31 +0000

    minuzie

--- trunk/htdocs/index.php
+++ trunk/htdocs/index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html>
+<html lang="it">
 <head>
 	<title>SongShuffler ~ $</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-15" />
@@ -7,12 +7,11 @@
 </head>
 
 <body>
-
 	<div id="header">
 		<a href="http://developer.berlios.de" title="BerliOS Developer">
-			<img src="http://developer.berlios.de/bslogo.php?group_id=6590" width="124px" height="32px" border="0" alt="BerliOS Developer Logo">
+			<img src="http://developer.berlios.de/bslogo.php?group_id=6590" alt="BerliOS Developer Logo" border="0" height="32" width="124" />
 		</a>
-	</div> <!-- /header -->
+	</div>	<!-- /header -->
 	
 	<div id="menu">
 		<ul>
@@ -23,17 +22,15 @@
 			<li>~/<a href="http://developer.berlios.de/project/memberlist.php?group_id=6590">contacts</a></li>
 			<!-- <li>~/credits</li> -->
 		</ul>
-	</div> <!-- /menu -->
+	</div>	<!-- /menu -->
 	
 	<div id="content">
-	<h1 id="maintitle">SongShuffler</h1>
-	<img src="img/work_in_progress.png" />
-	</div> <!-- /content -->
+		<h1 id="maintitle">SongShuffler</h1>
+		<img src="img/work_in_progress.png" />		
+	</div>	<!-- /content -->
 	
 	<div id="footer">
-		&nbsp;
-	</div> <!-- /footer -->
-
+		Last update: May 5, 2006. All site contents under the <a href="http://www.gnu.org/">GFDL</a> license.
+	</div>
 </body>
-
 </html>
