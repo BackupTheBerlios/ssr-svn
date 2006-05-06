887a416669238790d8f344f0c1b90a961b713e9a
rjack <rjack@d2ba497e-da12-0410-9649-cf4375da043f>
2006-05-06 17:22:22 +0000

    aggiustamenti al sito

--- trunk/htdocs/index.php
+++ trunk/htdocs/index.php
@@ -1,37 +1,41 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html lang="it">
 <head>
-	<title>SongShuffler ~ $</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-15" />
+	<title>SongShuffler home page</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<link rel="stylesheet" type="text/css" href="ssr.css" />
 </head>
 
 <body>
 	<div id="header">
-		<a href="http://developer.berlios.de" title="BerliOS Developer">
-			<img src="http://developer.berlios.de/bslogo.php?group_id=6590" alt="BerliOS Developer Logo" border="0" height="32" width="124" />
-		</a>
+		
 	</div>	<!-- /header -->
 	
 	<div id="menu">
+		<h1 id="maintitle">SongShuffler</h1>
 		<ul>
-			<li>~</li>
-			<li>~/<a href="http://developer.berlios.de/projects/ssr/">overview</a></li>
+			<li><a href="http://ssr.berlios.de/">~/</a></li>
+			<li>~/<a href="http://developer.berlios.de/projects/ssr/">sommario</a></li>
 			<!-- <li>~/download</li> -->
-			<li>~/<a href="http://svn.berlios.de/wsvn/ssr">code</a></li>
-			<li>~/<a href="doc">code/doc</a></li>
-			<li>~/<a href="http://developer.berlios.de/project/memberlist.php?group_id=6590">contacts</a></li>
+			<li>~/<a href="http://svn.berlios.de/wsvn/ssr">codice</a></li>
+			<li>~/<a href="doc">codice/doc</a></li>
+			<li>~/<a href="http://developer.berlios.de/project/memberlist.php?group_id=6590">contatti</a></li>
 			<!-- <li>~/credits</li> -->
 		</ul>
 	</div>	<!-- /menu -->
 	
 	<div id="content">
-		<h1 id="maintitle">SongShuffler</h1>
+		
 		<img src="img/work_in_progress.png" />		
 	</div>	<!-- /content -->
 	
 	<div id="footer">
-		Last update: May 5, 2006. All site contents under the <a href="http://www.gnu.org/">GFDL</a> license.
+		<a href="http://developer.berlios.de" title="BerliOS Developer">
+			<!-- l'id del progetto 6590 non deve essere modificato -->
+			<img id="berlioslogo" src="http://developer.berlios.de/bslogo.php?group_id=6590" alt="BerliOS Developer Logo" />
+		</a>
+		
+		Aggiornato il 6 maggio 2006. Tutti i contenuti del sito rilasciati sotto licenza <a href="http://www.gnu.org/">GFDL</a>.
 	</div>
 </body>
 </html>
